bbadc8bb594c6d09601cf340a0518c42fa2ab5ed
Rhody Lugo <rhodylugo@gmail.com>
2017-06-10 19:36:35 -0400

    add support for running only from the capture tool

--- CaptureTool/main.cpp
+++ CaptureTool/main.cpp
@@ -23,6 +23,7 @@
 #include "Utils.h"
 
 #import <CoreMIDI/CoreMIDI.h>
+#include <signal.h>
 
 #define INVALID_PID -1
 
@@ -34,6 +35,8 @@ char pktBuffer[1024];
 MIDIPacketList* pktList = (MIDIPacketList*) pktBuffer;
 MIDIPacket     *pkt;
 Byte            midiDataToSend[] = {0x91, 0x3c, 0x40};
+
+static volatile int keepRunning = 1;
 
 /**
  * Determines which process has a socket with the given endpoints. This returns
@@ -47,39 +50,39 @@ pid_t PIDForEndpoints(in_addr_t sourceAddress, int sourcePort, in_addr_t destAdd
 	static VMBuffer<pid_t> pidBuffer;
 	pidBuffer.Grow(proc_listpids(PROC_ALL_PIDS, 0, NULL, 0));
 	int pidCount = proc_listpids(PROC_ALL_PIDS, 0, pidBuffer.Data(), pidBuffer.Size()) / sizeof(pid_t);
-	
+
 	for(int i = 0; i < pidCount; i++) {
 		pid_t pid = pidBuffer[i];
-		
+
 		// We need to call proc_pidinfo once to get the size of the required buffer,
 		// then again to get the actual list.
 		static VMBuffer<proc_fdinfo> fdBuffer;
 		fdBuffer.Grow(proc_pidinfo(pid, PROC_PIDLISTFDS, 0, NULL, 0));
 		int fdCount = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, fdBuffer.Data(), fdBuffer.Size()) / sizeof(struct proc_fdinfo);
-		
+
 		for(int j = 0; j < fdCount; j++) {
 			// only interested in sockets
 			if(fdBuffer[j].proc_fdtype != PROX_FDTYPE_SOCKET) continue;
-			
+
 			// get the socket's info
 			socket_fdinfo finfo;
 			proc_pidfdinfo(pid, fdBuffer[j].proc_fd, PROC_PIDFDSOCKETINFO, &finfo, sizeof(finfo));
-			
+
 			// figure out this file's endpoints (holy nesting!)
 			int fdDestPort = finfo.psi.soi_proto.pri_in.insi_fport;
 			in_addr_t fdDestAddress = finfo.psi.soi_proto.pri_in.insi_faddr.ina_46.i46a_addr4.s_addr;
 			int fdSourcePort = finfo.psi.soi_proto.pri_in.insi_lport;
 			in_addr_t fdSourceAddress = finfo.psi.soi_proto.pri_in.insi_laddr.ina_46.i46a_addr4.s_addr;
-			
+
 			// see if this is our guy
 			if((sourceAddress == fdSourceAddress && sourcePort == fdSourcePort && destAddress == fdDestAddress && destPort == fdDestPort) ||
 			   (sourceAddress == fdDestAddress && sourcePort == fdDestPort && destAddress == fdSourceAddress && destPort == fdSourcePort)) {
 				return pid;
 			}
-			
+
 		}
 	}
-	
+
 	return INVALID_PID;
 }
 
@@ -94,20 +97,24 @@ pid_t PIDForEndpoints(in_addr_t sourceAddress, int sourcePort, in_addr_t destAdd
  */
 static void SendPacketData(const char *appPath, const struct pcap_pkthdr *packHead, const u_char *packData)
 {
+	if (gMessagePort == NULL) {
+		return;
+	}
+
 	static SInt32 msgid;
 	static VMBuffer<UInt8> messageBuffer;
 	messageBuffer.Grow(sizeof(pcap_pkthdr) + packHead->caplen + strlen(appPath) + 1);
-	
+
 	UInt8 *pos = messageBuffer.Data();
 	memcpy(pos, packHead, sizeof(pcap_pkthdr));
 	pos += sizeof(pcap_pkthdr);
-	
+
 	memcpy(pos, packData, packHead->caplen);
 	pos += packHead->caplen;
-	
+
 	memcpy(pos, appPath, strlen(appPath) + 1);
 	pos += strlen(appPath) + 1;
-	
+
 	CFDataRef data = CFDataCreateWithBytesNoCopy(NULL, messageBuffer.Data(), pos - messageBuffer.Data(), kCFAllocatorNull);
 	CFMessagePortSendRequest(gMessagePort, msgid++, data, -1, -1, NULL, NULL);
 	CFRelease(data);
@@ -119,14 +126,14 @@ static void SendPacketData(const char *appPath, const struct pcap_pkthdr *packHe
 void Handler(u_char *one, const struct pcap_pkthdr *packHead, const u_char *packData) {
 	const struct ether_header *etherHeader = (const struct ether_header *)packData;
 	const struct ip *ipHeader = (const struct ip *)(etherHeader + 1);
-	
+
 	// we can only find endpoints for TCP sockets for now
 	if(IPPROTO_UDP == ipHeader->ip_p) {
 		const struct tcphdr *tcpHeader = (const struct tcphdr *)(ipHeader + 1);
-		
+
 		// try to find our pid
 		pid_t owningProcess = PIDForEndpoints(ipHeader->ip_src.s_addr, tcpHeader->th_sport, ipHeader->ip_dst.s_addr, tcpHeader->th_dport);
-		
+
 		char buff[64];
 		unsigned char bytes[4];
 		bytes[0] = ipHeader->ip_src.s_addr & 0xFF;
@@ -134,7 +141,7 @@ void Handler(u_char *one, const struct pcap_pkthdr *packHead, const u_char *pack
 		bytes[2] = (ipHeader->ip_src.s_addr >> 16) & 0xFF;
 		bytes[3] = (ipHeader->ip_src.s_addr >> 24) & 0xFF;
 		sprintf(buff,"%d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);
-		
+
 		if(strcmp("192.168.0.101", buff) != 0) {
 			return;
 		}
@@ -149,27 +156,29 @@ void Handler(u_char *one, const struct pcap_pkthdr *packHead, const u_char *pack
 		pkt = MIDIPacketListAdd(pktList, 1024, pkt, 0, 3, midiDataToSend);
 		MIDIReceived(midiOut, pktList);
 
+		printf("%s\n", buff);
+
 		// did we find it?
-		if(INVALID_PID != owningProcess) {			
+		if(INVALID_PID != owningProcess) {
 			// grab the path
 			char processPath[MAXPATHLEN] = {};
 			proc_pidpath(owningProcess, processPath, sizeof(processPath));
-			
-			SendPacketData(buff, packHead, packData);
+
+			//SendPacketData(buff, packHead, packData);
 			return;
 		} else {
 			SendPacketData(buff, packHead, packData);
 			return;
 		}
 	}
-	
+
 	//SendPacketData("(unknown other)", packHead, packData);
 }
 
 bool SetupCapture(const char *interface) {
 	pcap_t *handle = pcap_open_live(interface, BUFSIZ, 1, 2, NULL);
 	if (handle == NULL) return false;
-	
+
 	// We can't run this on the main thread because it'll block, so we need to
 	// set it up on a background thread. I tried quite a bit to get this running
 	// under a CFRunloop using CFFileDescriptorRef, but it just wouldn't work.
@@ -180,7 +189,7 @@ bool SetupCapture(const char *interface) {
 		pcap_loop(handle, -1, Handler, NULL);
 		pcap_close(handle);
 	});
-	
+
 	return true;
 }
 
@@ -193,6 +202,10 @@ void MessagePortClosed(CFMessagePortRef ms, void *info) {
 	CFRunLoopStop(CFRunLoopGetCurrent());
 }
 
+void intHandler(int dummy) {
+	keepRunning = 0;
+}
+
 int main(int argc, char *argv[]) {
 	// The name of the message port we're supposed to be connecting to will be the
 	// first argument passed to the program. If it's invalid or not specified,
@@ -201,7 +214,7 @@ int main(int argc, char *argv[]) {
 		CFStringRef portName = CFStringCreateWithCString(NULL, argv[1], kCFStringEncodingUTF8);
 		gMessagePort = CFMessagePortCreateRemote(NULL, portName);
 		CFRelease(portName);
-		
+
 		if (!gMessagePort) return 1;
 	}
 
@@ -218,13 +231,18 @@ int main(int argc, char *argv[]) {
 		//
 		// Since we're in CF-land and not using Mach ports directly, we need a
 		// CFRunLoop and not dispatch_main.
-		CFMessagePortSetInvalidationCallBack(gMessagePort, MessagePortClosed);
-		CFRunLoopRun();
+		if (gMessagePort != NULL) {
+			CFMessagePortSetInvalidationCallBack(gMessagePort, MessagePortClosed);
+			CFRunLoopRun();
+		} else {
+			signal(SIGINT, intHandler);
+			while (keepRunning);
+		}
 
 		MIDIEndpointDispose(midiOut);
 		MIDIClientDispose(theMidiClient);
 		return 0;
 	}
-	
+
 	return 1;
 }
